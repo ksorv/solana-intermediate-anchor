116e568d3855e9dbb1b36e9395019d8c00a5d8ca
Saurav Kumar <sauravkhdoolia@gmail.com>
2024-06-26 17:36:50 +0530

    final program

--- programs/calculator/src/lib.rs
+++ programs/calculator/src/lib.rs
@@ -6,11 +6,49 @@ declare_id!("6RmZxF5CSUWq2FjREhGNoA8BQxbeHVvt7qZfxXWtsDU9");
 pub mod calculator {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create_account(ctx: Context<CreateAccount>) -> Result<()> {
+        ctx.accounts.counter_account.authority = ctx.accounts.authority.key();
+        ctx.accounts.counter_account.counter = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        ctx.accounts.counter_account.counter += 1;
+        Ok(())
+    }
+
+    pub fn decrement(ctx: Context<Decrement>) -> Result<()> {
+        ctx.accounts.counter_account.counter -= 1;
         Ok(())
     }
 }
 
+#[account]
+pub struct Counter {
+    pub authority: Pubkey,
+    pub counter: u64,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateAccount<'info> {
+    #[account(init, payer = authority, space = 8 + 8 + 32)]
+    pub counter_account: Account<'info, Counter>,
+    // the signer account must be mutable because it will be paying fee
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut, has_one = authority)]
+    pub counter_account: Account<'info, Counter>,
+    pub authority: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Decrement<'info> {
+    #[account(mut, has_one = authority)]
+    pub counter_account: Account<'info, Counter>,
+    pub authority: Signer<'info>,
+}
--- tests/calculator.ts
+++ tests/calculator.ts
@@ -1,16 +1,46 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Calculator } from "../target/types/calculator";
+import { Keypair, SystemProgram } from "@solana/web3.js"
 
 describe("calculator", () => {
+  const provider = anchor.AnchorProvider.env()
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Calculator as Program<Calculator>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  let counter = Keypair.generate()
+
+  it("Create Counter account!", async () => {
+    const tx = await program.methods.createAccount()
+        .accounts({
+          counterAccount: counter.publicKey,
+          authority: provider.wallet.publicKey,
+        })
+        .signers([counter])
+        .rpc()
+
+    console.log("Create account transaction signature", tx)
   });
+
+it("Can increase Counter!", async () => {
+    const tx = await program.methods.increment()
+        .accounts({
+            counterAccount: counter.publicKey,
+        })
+        .rpc()
+
+    console.log("Increment transaction signature", tx)
+});
+
+it("Can decrease Counter!", async () => {
+    const tx = await program.methods.decrement()
+        .accounts({
+            counterAccount: counter.publicKey,
+        })
+        .rpc()
+
+    console.log("Decrement transaction signature", tx)
+});
 });
